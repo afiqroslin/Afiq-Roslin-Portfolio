6eb385b6bf5de17474d5124a87570be8a1545fd1
Afiq <afiqroslin97@gmail.com>
2025-03-13 18:21:00 +0800

    new stack

---
+++ public/script.js
@@ -0,0 +1,19 @@
+
+$("#introduction").slideDown(2000);
+
+// Fade In automatically
+$(".content").fadeIn(2000);
+
+// Fade In when scroll down
+// $(document).ready(function () {
+//     $(window).on("scroll", function () {
+//         $("#experience").each(function () {
+//             let elementTop = $(this).offset().top;
+//             let windowBottom = $(window).scrollTop() + $(window).height();
+
+//             if (windowBottom > elementTop) {
+//                 $(this).addClass("fade-in");
+//             }
+//         });
+//     });
+// });
