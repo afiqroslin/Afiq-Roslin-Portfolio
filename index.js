642842a4d8b8719f78e05a17f626ecd0e8a080aa
Afiq <afiqroslin97@gmail.com>
2025-03-13 18:21:00 +0800

    new stack

--- index.js
+++ index.js
@@ -1,36 +1,18 @@
+import express from "express";
 
-$("#introduction").slideDown(2000);
-
-// Fade In automatically
-$("#experience").fadeIn(2000);
-
-// Fade In when scroll down
-// $(document).ready(function () {
-//     $(window).on("scroll", function () {
-//         $("#experience").each(function () {
-//             let elementTop = $(this).offset().top;
-//             let windowBottom = $(window).scrollTop() + $(window).height();
-
-//             if (windowBottom > elementTop) {
-//                 $(this).addClass("fade-in");
-//             }
-//         });
-//     });
-// });
-
-// show job description when click on job title
-$("#text-click-1").click(function(){
-    $(".exp-description-1").fadeToggle(1000);
-    });
-
-$("#text-click-2").click(function(){
-    $(".exp-description-2").fadeToggle(1000);
-    });
-
-$("#text-click-3").click(function(){
-    $(".exp-description-3").fadeToggle(1000);
-    });
+const app = express();
+const port = 3000;
 
 
+app.use(express.static("public"));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-    
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+
